06c1b3ce3e471be330f85ddb82551cc6d18e7bec
Martin Villavicencio <villavicencio.martin@protonmail.com>
2021-11-01 13:21:47 +0100

    added multiprocess to bilateralAdv

--- _tools/tool_bilateralAdv.py
+++ _tools/tool_bilateralAdv.py
@@ -1,13 +1,15 @@
 import os
+import multiprocessing as mp
 
 ################ MAKE CHANGES HERE #################
 inputFileFormat = "%03d"            # name of input files, e.g., %03d if files are named 001.png, 002.png
-imageFormat   = "input\\" + inputFileFormat + ".png"
-flowFwdFormat = "flow_fwd\\" + inputFileFormat + ".A2V2f"  # path to the forward flow files (computed by _tools/disflow)
-flowBwdFormat = "flow_bwd\\" + inputFileFormat + ".A2V2f"  # path to the backward flow files (computed by _tools/disflow)
-outputFormat   = "input_filtered\\" + inputFileFormat + ".png"  # path to the result filtered sequence
+imageFormat   = "input/" + inputFileFormat + ".png"
+flowFwdFormat = "flow_fwd/" + inputFileFormat + ".A2V2f"  # path to the forward flow files (computed by _tools/disflow)
+flowBwdFormat = "flow_bwd/" + inputFileFormat + ".A2V2f"  # path to the backward flow files (computed by _tools/disflow)
+outputFormat   = "input_filtered/" + inputFileFormat + ".png"  # path to the result filtered sequence
 frameFirst = 1                      # number of the first PNG file in the input folder
 frameLast = 109                     # number of the last PNG file in the input folder
+num_cores = mp.cpu_count()
 ####################################################
 
 firstFrame = frameFirst
@@ -16,7 +18,13 @@ frameStep = +1
 
 os.makedirs(os.path.dirname(outputFormat),exist_ok=True)
 
-for frame in range(firstFrame,lastFrame+frameStep,frameStep):  	
-  filter = "bilateralAdv.exe "+imageFormat+" "+flowFwdFormat+" "+flowBwdFormat+(" %d "%(frame))+" 15 16 "+(outputFormat%(frame))
-  #print(filter)
-  os.system(filter)
+def create_command(frame):
+  return "bilateralAdv.exe "+imageFormat+" "+flowFwdFormat+" "+flowBwdFormat+(" %d "%(frame))+" 15 16 "+(outputFormat%(frame))
+
+def execute_command(command):
+  os.system(command)
+
+commands = [create_command(frame) for frame in range(firstFrame, lastFrame + frameStep, frameStep)]
+
+pool = mp.Pool(num_cores)
+pool.map(execute_command, commands)
