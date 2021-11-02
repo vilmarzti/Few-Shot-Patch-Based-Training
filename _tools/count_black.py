64c2baa07266fc769f3a6f75838781e150286e26
Martin Villavicencio <villavicencio.martin@protonmail.com>
2021-11-02 10:25:07 +0100

    comments + mp in count_black

--- _tools/count_black.py
+++ _tools/count_black.py
@@ -4,21 +4,33 @@ import os
 import os.path as path
 import matplotlib.pyplot as plt
 import numpy as np
+import multiprocessing as mp
+
+cores = mp.cpu_count()
 
 
 def count_pixels(image_path):
+    # Count the number of black pixels in an image provided an image path
     image = cv2.imread(image_path, 0)
     num_black = image.size - cv2.countNonZero(image)
     return num_black
 
 
 def go_through_images(folder_path):
+    # Get image paths
     image_names = [f for f in os.listdir(folder_path) if path.isfile(path.join(folder_path, f))]
     image_names.sort()
     image_paths = [path.join(folder_path, f) for f in image_names]
-    num_blacks = [count_pixels(image) for image in image_paths]
+
+    # Count black pixels in images
+    pool = mp.Pool(cores)
+    num_blacks = pool.map(count_pixels, image_paths)
+
+    # Get statistics
     max_black = np.argmax(num_blacks)
     print(image_names[max_black])
+
+    # Output statistics
     plt.plot(num_blacks)
     plt.savefig("num_black.png")
 
