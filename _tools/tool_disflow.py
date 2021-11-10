02395876fe5b6017c148e3374e8ea6a601718d5a
Villavicencio Martin <villavicencio.martin@protonmail.com>
2021-11-10 21:51:26 +0100

    adjusted tool disflow for config

--- _tools/tool_disflow.py
+++ _tools/tool_disflow.py
@@ -1,33 +1,23 @@
 import os
 import multiprocessing as mp
+import config
 
-################ MAKE CHANGES HERE #################
-inputDir = "/home/martin/Videos/ondrej_et_al/bf/bf_gen/input_filtered/"              # path to the input sequence PNGs
-inputFileFormat = "%04d"        # name of input files, e.g., %03d if files are named 001.png, 002.png
-inputFileExt = "png"            # extension of input files (without .), e.g., png, jpg
-flowFwdDir = "../TEST_DATA/bf_gen/flow_fwd"         # path to the output forward flow files
-flowBwdDir = "../TEST_DATA/bf_gen/flow_bwd"         # path to the output backward flow files
-FIRST = 1                       # number of the first PNG file in the input folder
-LAST = 1417                      # number of the last PNG file in the input folder
+FIRST = int(config.frameFirst)                      # number of the first PNG file in the input folder
+LAST = int(config.frameLast)
 num_cores = mp.cpu_count()
-####################################################
+command_string = config.disflowExecutable + " %s %s %s"
 
-
-if not os.path.exists(flowFwdDir):
-    os.mkdir(flowFwdDir)
+if not os.path.exists(config.flowFwdDir):
+    os.mkdir(config.flowFwdDir)
     
-if not os.path.exists(flowBwdDir):
-    os.mkdir(flowBwdDir)
-
-inputFiles = inputDir + "/" + inputFileFormat + "." + inputFileExt
-flwFwdFile = flowFwdDir + "/" + inputFileFormat + ".A2V2f"
-flwBwdFile = flowBwdDir + "/" + inputFileFormat + ".A2V2f"
+if not os.path.exists(config.flowBwdDir):
+    os.mkdir(config.flowBwdDir)
 
 def create_commands(frame, fwd=True):
   if fwd:
-    command = "./disflow/disflow %s %s %s"%(inputFiles%(frame),inputFiles%(frame-frameStep),flwFwdFile%(frame)) 
+    command = command_string % (config.imageFormat%(frame), config.imageFormat % (frame-frameStep), config.flowFwdFiles % (frame)) 
   else:
-    command = "./disflow/disflow %s %s %s"%(inputFiles%(frame),inputFiles%(frame-frameStep),flwBwdFile%(frame)) 
+    command = command_string % (config.imageFormat%(frame), config.imageFormat % (frame-frameStep), config.flowBwdFiles%(frame)) 
   return command
 
 pool = mp.Pool()
