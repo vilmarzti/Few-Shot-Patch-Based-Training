05867f1d745ed70c91caebcb213d075df2d73c97
Villavicencio Martin <villavicencio.martin@protonmail.com>
2021-11-10 20:12:09 +0100

    some fixes to config.py

--- _tools/config.py
+++ _tools/config.py
@@ -1,16 +1,23 @@
-
-data_folder = "/home/martin/Documents/code/python/Few-Shot-Patch-Based-Training/TEST_DATA/"
+from os import path
+data_folder = "~/Videos/ondrej_et_al/bf"
 inputFileFormat = "%04d"    # name of input files, e.g., %03d if files are named 001.png, 002.png
-maskDir = data_folder + "/bf_train/mask_gauss_10"            # mask dir, essentially leading frames from where the gaussians will be propagated
-maskFiles = maskDir + "/" + inputFileFormat + ".png"
-flowFwdFiles = data_folder + "/bf_gen/flow_fwd" + "/" + inputFileFormat + ".A2V2f"  # path to the forward flow files (computed by _tools/disflow)
-flowBwdFiles = data_folder +"/bf_gen/flow_bwd" + "/" + inputFileFormat + ".A2V2f"  # path to the backward flow files (computed by _tools/disflow)
+
+inputPNG = inputFileFormat + ".png"
+
+maskDir = path.join(data_folder, "/bf_train/mask_gauss_10")        # mask dir, essentially leading frames from where the gaussians will be propagated
+maskFiles = path.join(maskDir,  inputFileFormat + ".png")
+
+flowFwdFiles = path.join(data_folder, "/bf_gen/flow_fwd", inputFileFormat + ".A2V2f")  # path to the forward flow files (computed by _tools/disflow)
+flowBwdFiles = path.join(data_folder, "/bf_gen/flow_bwd", inputFileFormat + ".A2V2f")  # path to the backward flow files (computed by _tools/disflow)
+
 frameFirst = "0001"                  # name of the first PNG file in the input folder (without extension)
 frameLast = "1417"                   # number of the last PNG file in the input folder (without extension)
-gdisko_gauss_r10_s10_dir = data_folder + "bf_gen/input_gdisko_gauss_r10_s10"    # path to the result gauss r10 s10 sequence
-gdisko_gauss_r10_s15_dir = data_folder + "bf_gen/input_gdisko_gauss_r10_s15"    # path to the result gauss r10 s15 sequence
-gdisko_gauss_r10_s10_files = gdisko_gauss_r10_s10_dir + "/" + inputFileFormat + ".png" 
-gdisko_gauss_r10_s15_files = gdisko_gauss_r10_s15_dir + "/" + inputFileFormat + ".png" 
-imageFormat   = "../TEST_DATA/bf_gen/input/" + inputFileFormat + ".png"
 frameStep = +1
-bilateralOutputFormat  = "../TEST_DATA/bf_gen/input_filtered/" + inputFileFormat + ".png"  # path to the result filtered sequence
+
+gdisko_gauss_r10_s10_dir = path.join(data_folder,"bf_gen/input_gdisko_gauss_r10_s10")    # path to the result gauss r10 s10 sequence
+gdisko_gauss_r10_s15_dir = path.join(data_folder,"bf_gen/input_gdisko_gauss_r10_s15")    # path to the result gauss r10 s15 sequence
+gdisko_gauss_r10_s10_files = path.join(gdisko_gauss_r10_s10_dir, inputPNG)
+gdisko_gauss_r10_s15_files = path.join(gdisko_gauss_r10_s15_dir, inputPNG) 
+
+imageFormat   = path.join(data_folder, "/bf_gen/input/", inputPNG)
+bilateralOutputFormat  = path.join(data_folder, "bf_gen/input_filtered/", inputPNG)  # path to the result filtered sequence
